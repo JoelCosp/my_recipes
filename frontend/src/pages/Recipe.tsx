82fd43e8e45d7d3ea9e64012578d6a5e2fe043a2
JoelVRV <joelc456789@gmail.com>
2025-03-04 22:44:52 +0100

    ---

--- frontend/src/pages/Recipe.tsx
+++ frontend/src/pages/Recipe.tsx
@@ -30,12 +30,14 @@ const RecipePage = () => {  // Cambié el nombre del componente
       {recipe ? ( // Asegurarnos de que recipe no sea null antes de acceder a sus datos
         <div className='max-w-[1240px] mx-auto px-5'>
           <div className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
-            <img className='rounded-t-2xl rounded-b-2xl' src={recipe[0].img_url} alt="" />
+          <div className='flex items-stretch overflow-hidden h-full'>
+            <img className='w-full h-full object-cover rounded-t-2xl rounded-b-2xl' src={recipe[0].img_url} alt="" />
+          </div>
             <div>
               <h3 className='font-bold uppercase text-2xl pb-5 mb-5 border-b-2 border-[#afafaf]'>{recipe[0].name}</h3>
               <p className='text-justify pb-5'>{recipe[0].description}</p>
               <p className='flex gap-2 pb-5'><ChefHat size={25} color={"#ffad4a"}/><b>NIVEL DE DIFICULTAD:</b> {recipe[0].difficulty}</p>
-              <p className='flex gap-2 pb-5'><Clock size={25} color={"#ffad4a"}/><b>DURACION:</b> {recipe[0].time} min</p>
+              <p className='flex gap-2'><Clock size={25} color={"#ffad4a"}/><b>DURACION:</b> {recipe[0].time} min</p>
             </div>
           </div>
           <h4 className='font-bold uppercase text-xl py-5 border-b-1'>Ingredients:</h4>
