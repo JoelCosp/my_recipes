8ad297004cfb2c1787061d859669751833c17f0f
JoelVRV <joelc456789@gmail.com>
2025-03-05 18:03:41 +0100

    ---

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -4,19 +4,33 @@ import { Vegan } from 'lucide-react';
 
 const Nav = () => {
   return (
-    <nav className='mb-5'>
-        <div className='px-5 max-w-[1240px] mx-auto py-[30px] flex items-center justify-center flex-col'>
-            <Vegan size={60} color="black" />
-            <div className='border-b-1 border-[#c5c5c5] w-full flex items-center justify-center'>
-              <ul className='flex text-[#212121] font-semibold'>
-                  <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/">RECETAS</Link></li>
-                  <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/ingredients">INGREDIENTES</Link></li>
-                  <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/surprise">SORPRESA</Link></li>
-                  <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/create">CREAR</Link></li>
-              </ul>
-            </div>
+    <>
+      <nav className='mb-5 hidden sm:block'>
+          <div className='px-5 max-w-[1240px] mx-auto py-[30px] flex items-center justify-center flex-col'>
+              <Vegan size={60} color="#212121" />
+              <div className='border-b-1 border-[#c5c5c5] w-full flex items-center justify-center'>
+                <ul className='flex text-[#212121] font-semibold'>
+                    <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/">RECETAS</Link></li>
+                    <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/ingredients">INGREDIENTES</Link></li>
+                    <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/surprise">SORPRESA</Link></li>
+                    <li className='px-4 py-[30px] hover:text-[#ffad4a]'><Link to="/create">CREAR</Link></li>
+                </ul>
+              </div>
+          </div>
+      </nav>
+      <nav className='sm:hidden relative'>
+        <div className='absolute bg-white left-0 right-15 top-0 h-screen'>
+          <ul className='p-5 font-semibold text-[#212121]'>
+            <li className='py-2'><Vegan size={40} color="#212121" /></li>
+            <li className='py-2'><Link to="/">RECETAS</Link></li>
+            <li className='py-2'><Link to="/ingredients">INGREDIENTES</Link></li>
+            <li className='py-2'><Link to="/surprise">SORPRESA</Link></li>
+            <li className='py-2'><Link to="/create">CREAR</Link></li>
+          </ul>
         </div>
-    </nav>
+      </nav>
+    </>
+    
   )
 }
 
--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Category from '../models/Category';
+import Ingredient from '../models/Ingredient';
 
 const Create = () => {
 
     const [categories, setCategories] = useState<Category[]>([]);
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
     const [newRecipe, setNewRecipe] = useState(null);
 
     const [formData, setFormData] = useState({
@@ -20,6 +22,12 @@ const Create = () => {
         axios.get<Category[]>('http://localhost:5000/categories')
         .then((response) => {
             setCategories(response.data);
+        })
+        .catch(console.error);
+
+        axios.get<Ingredient[]>('http://localhost:5000/ingredients')
+        .then((response) => {
+            setIngredients(response.data);
         })
         .catch(console.error);
     }, [])
@@ -45,39 +53,47 @@ const Create = () => {
   return (
     <section>
         <div className='max-w-[1240px] mx-auto px-5 flex justify-center'>
-            <form className='flex flex-col' onSubmit={handleSubmit}>
-                <h2 className='font-bold text-2xl pb-5 text-[#212121]'>NUEVA RECETA</h2>
-                <input className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="name" type="text" placeholder='Nombre' onChange={handleChange}/>
-                <input className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="description" type="text" placeholder='Descripción' onChange={handleChange}/>
-                <input className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="time" type="number" min="0" placeholder='Duración' onChange={handleChange}/>
-                <input className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="img_url" type="url" placeholder='Url' onChange={handleChange}/>
-                <select className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="category_id" onChange={handleChange}>
-                    {
-                        categories.map((category) => {
-                            return (
-                                <option key={category.id} className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value={category.id}>{category.name}</option>
-                            )
-                        })
-                    }
-                </select>
-                <div className='flex mb-5'>
-                    <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Baja" onChange={handleChange}/>Baja</label>
-                    <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Media" onChange={handleChange}/>Media</label>
-                    <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Alta" onChange={handleChange}/>Alta</label>
-                </div>
-                <select className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="category_id" onChange={handleChange}>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">1</option>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">2</option>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">3</option>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">4</option>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">5</option>
-                    <option className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value="x">6</option>
-                </select>
-                <div className='flex justify-center gap-5 font-semibold'>
-                    <button className='px-5 w-full rounded-2xl py-3 border-1 border-[red] text-red-600 hover:cursor-pointer hover:bg-red-600 hover:text-white'>CANCELAR</button>
-                    <button className='px-5 bg-[#4fa909] w-full rounded-2xl py-3 text-white hover:cursor-pointer hover:bg-[#3b8700]'>CREAR</button>
-                </div>
-            </form>
+            <div className=''>
+                <form className='grid grid-cols-1 sm:grid-cols-2 gap-5' onSubmit={handleSubmit}>
+                    <div>
+                        <h2 className='font-bold text-2xl pb-5 text-[#212121]'>NUEVA RECETA</h2>
+                        <input className='w-full px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="name" type="text" placeholder='Nombre' onChange={handleChange}/>
+                        <input className='w-full px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="description" type="text" placeholder='Descripción' onChange={handleChange}/>
+                        <input className='w-full px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="time" type="number" min="0" placeholder='Duración' onChange={handleChange}/>
+                        <input className='w-full px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="img_url" type="url" placeholder='Url' onChange={handleChange}/>
+                        <select className='w-full px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' name="category_id" onChange={handleChange}>
+                            {
+                                categories.map((category) => {
+                                    return (
+                                        <option key={category.id} className='w-[400px] px-5 py-5 mb-5 rounded-2xl outline-hidden bg-[#dadada]' value={category.id}>{category.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        <div className='flex mb-5'>
+                            <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Baja" onChange={handleChange}/>Baja</label>
+                            <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Media" onChange={handleChange}/>Media</label>
+                            <label className='mr-5 hover:cursor-pointer'><input className='mr-1 hover:cursor-pointer' type="radio" name="difficulty" value="Alta" onChange={handleChange}/>Alta</label>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className='text-xl text-[#212121] font-bold mb-5'>SELECCIONA LOS INGREDIENTES: </h3>
+                        <select className='outline-hidden' name="category_id" onChange={handleChange} multiple>
+                            {
+                                ingredients.map((ingredient) => {
+                                    return (
+                                        <option className='w-[400px] px-5 py-5 mb-5 outline-hidden' value={ingredient.id}>{ingredient.id}. {ingredient.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className='flex justify-center gap-5 font-semibold'>
+                        <button className='px-5 w-full rounded-2xl py-3 border-1 border-[red] text-red-600 hover:cursor-pointer hover:bg-red-600 hover:text-white'>CANCELAR</button>
+                        <button className='px-5 bg-[#4fa909] w-full rounded-2xl py-3 text-white hover:cursor-pointer hover:bg-[#3b8700]'>CREAR</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
   )
